6f0a01093cab03ac66084db392e801f42ae3d662
Qiran Xiao <sys2010xiaoqiran@126.com>
2026-03-18 01:58:25 +0800

    feat: add editUrl configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/campass-tour/campass-tour.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -55,7 +55,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/campass-tour/campass-tour.github.io/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -89,7 +89,7 @@ const config: Config = {
         // },
         // {to: '/blog', label: 'Blog', position: 'left'},
         // {
-        //   href: 'https://github.com/facebook/docusaurus',
+        //   href: 'https://github.com/campass-tour/campass-tour.github.io',
         //   label: 'GitHub',
         //   position: 'right',
         // },
@@ -229,7 +229,7 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/campass-tour/campass-tour.github.io',
             },
           ],
         },
